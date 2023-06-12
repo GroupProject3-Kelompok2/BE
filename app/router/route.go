7d8337d2c33f6395a9a140c56eda2cccc0df9351
dimasyudhana <dimas.yudhana@gmail.com>
2023-06-13 00:38:23 +0700

    feature/users-put/users/:user_id

--- app/router/route.go
+++ app/router/route.go
@@ -30,4 +30,5 @@ func initUserRouter(db *gorm.DB, e *echo.Echo) {
 	e.POST("/register", userHandler.Register())
 	e.POST("/login", userHandler.Login())
 	e.GET("/users/:id", userHandler.ProfileUser(), middlewares.JWTMiddleware())
+	e.PUT("/users/:id", userHandler.UpdateUser(), middlewares.JWTMiddleware())
 }
