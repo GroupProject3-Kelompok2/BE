40d3e008c9c614b30cc33626842183703b0d84b7
belhiibeng <belhi.romdona@gmail.com>
2023-06-13 08:30:06 +0700

    feat: add homestay

--- features/homestay/entity.go
+++ features/homestay/entity.go
@@ -2,21 +2,24 @@ package homestay
 
 import (
 	"time"
-
-	"github.com/GroupProject3-Kelompok2/BE/features/review"
-	"gorm.io/gorm"
 )
 
 type HomestayCore struct {
-	HomestayID       string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-	DeletedAt        gorm.DeletedAt
-	Hoster           string
-	HomestayPictures []HomestayPictureCore
-	Reviews          []review.ReviewCore
+	HomestayID   string
+	Hoster       string  `validate:"required"`
+	Name         string  `validate:"required"`
+	Descriptions string  `validate:"required"`
+	Address      string  `validate:"required"`
+	Price        float64 `validate:"required"`
+	Status       bool
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
 }
 
-type HomestayPictureCore struct {
-	HomestayPicture string
+type HomestayDataInterface interface {
+	Insert(input HomestayCore) (uint64, error)
+}
+
+type HomestayServiceInterface interface {
+	Create(input HomestayCore) (HomestayCore, error)
 }
