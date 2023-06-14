7cc9f453987e82e2dca0630ccdbc4e0320eb36df
belhiibeng <belhi.romdona@gmail.com>
2023-06-14 12:32:33 +0700

    feat: add homestay with picture

--- features/homestay/handler/response.go
+++ features/homestay/handler/response.go
@@ -8,7 +8,6 @@ type HomestayResponse struct {
 	Description string  `json:"description,omitempty" form:"description"`
 	Address     string  `json:"address,omitempty" form:"address"`
 	Price       float64 `json:"price,omitempty" form:"price"`
-	Status      bool    `json:"status,omitempty" form:"status"`
 }
 
 func HomestayCoreResponse(homestay homestay.HomestayCore) HomestayResponse {
